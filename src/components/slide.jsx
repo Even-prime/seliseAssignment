611c1cd7628fe256df0f089f3bf9e21de92915a7
Lupon <2004042@student.ruet.ac.bd>
2025-04-13 19:09:02 +0600

    Add client folder to repository

---
+++ src/components/slide.jsx
@@ -0,0 +1,14 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Welcome Home! A place to stay, <br /> a place to belong.
+        Make every journey feel like home.
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
---
+++ src/pages/ListingDetails.jsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+// import { useEffect, useState } from "react";
+// import "../styles/ListingDetails.scss";
+// import { useNavigate, useParams } from "react-router-dom";
+// import { facilities } from "../data";
+
+// // import "react-date-range/dist/styles.css";
+// // import "react-date-range/dist/theme/default.css";
+// // import { DateRange } from "react-date-range";
+// // import Loader from "../components/Loader";
+// // import Navbar from "../components/Navbar";
+// // import { useSelector } from "react-redux";
+// // // import Footer from "../components/Footer"
+
+
+// const ListingDetails = () => {
+//     const [loading, setLoading] = useState(true)
+//   const {listingId} = useParams()
+//   const [listing , setListing] =useState(null)
+
+//     return (
+//       <div className='listing-details'>
+//         <div className="title">
+//           <h1></h1>
+//         </div>
+//       </div>
+//     )
+//   }
+  
+//   export default ListingDetails
+  
